ec9f538ef9c1fab80fcde8b077226f470600a5bd
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-22 19:36:21 -0800

    Added animation to return apple to origin.

--- Phonagotchi/LPGViewController.m
+++ Phonagotchi/LPGViewController.m
@@ -8,14 +8,17 @@
 
 #import "LPGViewController.h"
 
-@interface LPGViewController ()
+@interface LPGViewController () <UIGestureRecognizerDelegate>
 
 @property (nonatomic) UIImageView *petImageView;
 @property (nonatomic) Pet *pet;
+@property (nonatomic) BOOL isAppleBeingPinched;
+@property CGRect originalFrame;
+@property (nonatomic) IBOutlet UIImageView *appleView;
 
 @end
 
-@implementation LPGViewController
+@implementation LPGViewController 
 
 - (void)viewDidLoad
 {
@@ -30,7 +33,9 @@
     
     self.petImageView.image = [UIImage imageNamed:@"default"];
     
+    
     [self.view addSubview:self.petImageView];
+    [self.view bringSubviewToFront:self.appleView];
     
     [NSLayoutConstraint constraintWithItem:self.petImageView
                                   attribute:NSLayoutAttributeCenterX
@@ -49,10 +54,63 @@
                                    constant:0.0].active = YES;
     
     self.petImageView.userInteractionEnabled = YES;
+    self.appleView.userInteractionEnabled = YES;
     
     UIPanGestureRecognizer *pettingRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePetting:)];
     [self.petImageView addGestureRecognizer:pettingRecognizer];
+    
+
 }
+
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+    return YES;
+}
+
+
+
+- (IBAction)panApple:(UIPanGestureRecognizer *)sender {
+    
+    
+    CGPoint appleLocation = [sender locationInView:self.view];
+    self.appleView.center = appleLocation;
+    
+        switch(sender.state) {
+            case UIGestureRecognizerStateBegan:
+                self.originalFrame = self.appleView.frame;
+                break;
+                
+            
+            case UIGestureRecognizerStateEnded: {
+                if(CGRectContainsPoint(self.petImageView.frame, appleLocation)) {
+                    self.appleView.alpha = 0;
+                    
+                    [UIView animateWithDuration:2 animations:^{
+                        self.appleView.frame = self.originalFrame;
+                    }completion:^(BOOL finished) {
+                        self.appleView.alpha = 1;
+                    }];
+                    
+                } else {
+                    [UIView animateWithDuration:2 animations:^{
+                        CGFloat newYLocation = CGRectGetMaxY(self.view.frame) + CGRectGetHeight(self.appleView.frame);
+                        
+                        self.appleView.center = CGPointMake(self.appleView.frame.origin.x, newYLocation);
+                        
+                    } completion:^(BOOL finished) {
+                        self.appleView.frame = self.originalFrame;
+                        self.appleView.alpha = 1;
+                    }];
+                }
+                
+                break;
+            }
+            default:
+                break;
+        }
+    
+    
+}
+
 
 -(void)handlePetting:(UIPanGestureRecognizer*)pettingRecognizer {
 
@@ -70,3 +128,6 @@
 }
 
 @end
+
+
+
